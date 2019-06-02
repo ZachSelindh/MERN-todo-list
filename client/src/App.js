9414a80ae7290e62a6e49fd3fcd623e259de23eb
Zach Selindh <zachselindh@gmail.com>
2019-06-02 15:22:14 -0500

    Added/styled header, added controller.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <Wrapper />;
+    return (
+      <Wrapper>
+        <Header />
+      </Wrapper>
+    );
   }
 }
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,73 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const todoController = require("../../controllers/todoController");
 
-const Todo = require("../../models/toDoItem");
+router
+  .route("/")
+  .get(todoController.findAll)
+  .post(todoController.create);
 
-// @route   GET api/todos
-// @desc    Get all todos
-// @access   Public
-router.get("/", (req, res) => {
-  Todo.find({}).then(todo => res.send(todo));
-});
-
-// @route   POST api/todos
-// @desc    Create a Todo
-// @access   Public
-router.post("/", (req, res) => {
-  const {
-    title,
-    author,
-    description,
-    completed,
-    submitted_at,
-    completed_at
-  } = req.body;
-  const newTodo = new Todo({
-    title,
-    author,
-    description,
-    completed,
-    submitted_at,
-    completed_at
-  });
-  newTodo
-    .save()
-    .then(console.log(newTodo))
-    .then(todo => res.json(todo))
-    .catch(err => console.log(err));
-});
-
-// @route   PUT api/todos
-// @desc    Update a Todo
-// @access   Public
-router.put("/:id", (req, res) => {
-  const {
-    title,
-    author,
-    description,
-    completed,
-    submitted_at,
-    completed_at
-  } = req.body;
-  console.log("Put/update: ", req.params.id);
-  Todo.findByIdAndUpdate(
-    { _id: req.params.id },
-    { $set: title, author, description, completed, submitted_at, completed_at },
-    function(err, res) {
-      if (err) throw err;
-      console.log(res);
-    }
-  );
-});
-
-// @route   DELETE api/todo/id
-// @desc    Delete a Todo
-// @access   Public
-router.delete("/:id", (req, res) => {
-  console.log("Delete: ", req.params.id);
-  Todo.findById({ _id: req.params.id })
-    .then(todo => todo.remove())
-    .catch(err => res.status(422).json(err));
-});
+router
+  .route("/:id")
+  .put(todoController.update)
+  .delete(todoController.remove);
 
 module.exports = router;
